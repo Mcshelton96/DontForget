e5809b24582f5d91189ffde38b1484dd245872f4
Mcshelton96 <mcshelton96@gmail.com>
2023-05-20 10:26:32 -0500

    Added Contact Model

---
+++ Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DontForget.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string ContactName { get; set; }
+
+        [Required]
+        public string? ContactMembers { get; set; }
+
+        [Required]
+        public string ContactAddress { get; set; }
+
+        public DateOnly ContactBirthday { get; set; }
+
+    }
+}
