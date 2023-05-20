648eee405e0c5470898a7e0f84a56981fbeae31f
Mcshelton96 <mcshelton96@gmail.com>
2023-05-20 11:27:00 -0500

    User Repo with CRUD

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,7 +1,147 @@
-﻿namespace DontForget.Repositories
-{
-    public class UserRepository
-    {
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using DontForget.Models;
+using DontForget.Utils;
 
+namespace DontForget.Repositories
+{
+    public class UserRepository : BaseRepository
+    {
+        public UserRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<User> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                          SELECT Id, Name, userName, Email, Address
+                            FROM User
+                            "
+                    ;
+
+                    var reader = cmd.ExecuteReader();
+
+                    var users = new List<User>();
+                    while (reader.Read())
+                    {
+                        users.Add(new User()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            Name = DbUtils.GetString(reader, "Name"),
+                            userName = DbUtils.GetString(reader, "userName"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            Address = DbUtils.GetString(reader, "Address"),
+                        });
+                    }
+
+                    reader.Close();
+
+                    return users;
+                }
+            }
+        }
+
+        public User GetById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                          SELECT Id, Name, userName, Email, Address
+                            FROM User
+                            ";
+
+                    DbUtils.AddParameter(cmd, "@Id", id);
+
+                    var reader = cmd.ExecuteReader();
+
+                    User user = null;
+                    if (reader.Read())
+                    {
+                        user = new User()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            Name = DbUtils.GetString(reader, "Name"),
+                            userName = DbUtils.GetString(reader, "userName"),
+                            Email = DbUtils.GetString(reader, "Email"),
+                            Address = DbUtils.GetString(reader, "Address"),
+                        };
+                    }
+
+                    reader.Close();
+
+                    return user;
+                }
+            }
+        }
+
+        public void Add(User user)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO User (Name, userName, Email, Address)
+                        OUTPUT INSERTED.ID
+                        VALUES (@Name, @userName, @Email, @Address)";
+
+                    DbUtils.AddParameter(cmd, "@Name", user.Name);
+                    DbUtils.AddParameter(cmd, "@userName", user.userName);
+                    DbUtils.AddParameter(cmd, "@Email", user.Email);
+                    DbUtils.AddParameter(cmd, "@Address", user.Address);
+
+                    user.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public void Update(User user)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        UPDATE Post
+                           SET Name = @Name,
+                               username = @userName,
+                               Email = @Email,
+                               Address = @Address,
+                         WHERE Id = @Id";
+
+                    DbUtils.AddParameter(cmd, "@Name", user.Name);
+                    DbUtils.AddParameter(cmd, "@userName", user.userName);
+                    DbUtils.AddParameter(cmd, "@Email", user.Email);
+                    DbUtils.AddParameter(cmd, "@Address", user.Address);
+                    DbUtils.AddParameter(cmd, "@Id", user.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Post WHERE Id = @Id";
+                    DbUtils.AddParameter(cmd, "@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
