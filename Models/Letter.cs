343055ec96de92a1772e668dfd0bb229ae7188df
Mcshelton96 <mcshelton96@gmail.com>
2023-05-20 10:30:10 -0500

    Added Letter Model

---
+++ Models/Letter.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DontForget.Models
+{
+    public class Letter
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int userId { get; set; }
+
+        [Required]
+        public string LetterTitle { get; set; }
+
+        [Required]
+        public string LetterBody { get; set;}
+    }
+}
