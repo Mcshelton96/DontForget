ed9536dc42a619d4a8c91fadc0d1ae3943e04d18
Mcshelton96 <mcshelton96@gmail.com>
2023-05-22 19:41:43 -0500

    User Controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DontForget.Repositories;
+using DontForget.Models;
+using Microsoft.Extensions.Hosting;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DontForget.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_userRepository.GetAllUsers());
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = _userRepository.GetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public IActionResult User(User user)
+        {
+            _userRepository.Add(user);
+            return CreatedAtAction("Get", new { id = user.Id }, user);
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _userRepository.Update(user);
+            return NoContent();
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
